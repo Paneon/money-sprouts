68b6c513dc74c1b0c575f2f20c775497e7a7c6e1
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2023-07-31 22:09:57 +0200

    #2 adjust styles.scss to conform with scss guidelines, add images for overview page, adjust module import/exports to fix CommonModule issue

--- apps/client/src/app/pages/pages.module.ts
+++ apps/client/src/app/pages/pages.module.ts
@@ -8,13 +8,12 @@ import { TransactionHistoryComponent } from './transaction-history/transaction-h
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { SpendingsComponent } from './spendings/spendings.component';
-
-const components = [UserSelectionComponent];
-const modules = [CommonModule, SharedModule];
+import { UserService } from '../services/user.service';
 
 @NgModule({
+  imports: [CommonModule, SharedModule],
   declarations: [
-    ...components,
+    UserSelectionComponent,
     StartComponent,
     BalanceOverviewComponent,
     TransactionHistoryComponent,
@@ -23,9 +22,18 @@ const modules = [CommonModule, SharedModule];
     UserSelectionComponent,
     SpendingsComponent,
   ],
-  imports: [...modules],
-  exports: [...components, ...modules],
+  exports: [
+    UserSelectionComponent,
+    StartComponent,
+    BalanceOverviewComponent,
+    TransactionHistoryComponent,
+    DashboardComponent,
+    LoginComponent,
+    UserSelectionComponent,
+    SpendingsComponent,
+    CommonModule
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [],
+  providers: [UserService],
 })
 export class PagesModule {}
--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,11 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { environment } from "./environments/environments";
+import { environment } from './environments/environments';
 import { AppModule } from './app/app.module';
 
-if(environment.production){
+if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
