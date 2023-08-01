b05f446574652289b47d00371387d796d9c8095e
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2023-08-01 17:34:03 +0200

    Fixed avatar disappearance issue with Observable-based route detection

--- apps/client/src/shared/user-avatar/user-avatar.component.ts
+++ apps/client/src/shared/user-avatar/user-avatar.component.ts
@@ -1,33 +1,49 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subject, filter, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'money-sprouts-user-avatar',
-  templateUrl: './user-avatar.component.html',
-  styleUrls: ['./user-avatar.component.scss'],
+    selector: 'money-sprouts-user-avatar',
+    templateUrl: './user-avatar.component.html',
+    styleUrls: ['./user-avatar.component.scss'],
 })
-export class UserAvatarComponent implements OnInit {
-  @Input() avatarFile = '';
-  urlSegment = '';
-  smallPaths: string[] = ['dashboard', 'overview', 'history', 'plan'];
+export class UserAvatarComponent implements OnInit, OnDestroy {
+    @Input() avatarFile = '';
+    urlSegment = '';
+    smallPaths: string[] = ['dashboard', 'overview', 'history', 'plan'];
+    destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(private router: Router) {}
+    constructor(private router: Router) {}
 
-  ngOnInit() {
-    this.urlSegment = this.router.url.split('/')[2];
-  }
-
-  getAvatarFile(): string {
-    return this.avatarFile;
-  }
-
-  getClass(): string {
-    if (this.urlSegment === 'userselection') {
-      return 'avatar-icon';
-    } else if (this.smallPaths.includes(this.urlSegment)) {
-      return 'avatar-icon-small';
-    } else {
-      return '';
+    ngOnInit() {
+        this.urlSegment = this.router.url.split('/')[2];
+        this.router.events
+            .pipe(
+                filter((event) => event instanceof NavigationEnd),
+                takeUntil(this.destroy$)
+            )
+            .subscribe((event: NavigationEnd) => {
+                this.urlSegment = event.url.split('/')[2];
+            });
     }
-  }
+
+    ngOnDestroy() {
+        this.destroy$.next(true);
+        this.destroy$.unsubscribe();
+    }
+
+    getClass() {
+        if (this.urlSegment === 'userselection') {
+            return 'avatar-icon';
+        } else if (this.smallPaths.includes(this.urlSegment)) {
+            console.log('blub-small');
+            return 'avatar-icon--small';
+        } else {
+            return '';
+        }
+    }
+
+    getAvatarFile() {
+        return this.avatarFile;
+    }
 }
