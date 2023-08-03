8fccc4aec323a25c6c49d8b64b1d04ac1afd6dc0
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2023-08-03 16:11:00 +0200

    #12 solve layout issue by introducing mixins for custom container and text-box

--- apps/client/src/app/pages/pages.module.ts
+++ apps/client/src/app/pages/pages.module.ts
@@ -9,34 +9,31 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { SpendingsComponent } from './spendings/spendings.component';
 import { UserService } from '../services/user.service';
-import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-    imports: [CommonModule, SharedModule],
-    declarations: [
-        UserSelectionComponent,
-        StartComponent,
-        BalanceOverviewComponent,
-        TransactionHistoryComponent,
-        DashboardComponent,
-        LoginComponent,
-        UserSelectionComponent,
-        SpendingsComponent,
-        LayoutComponent,
-    ],
-    exports: [
-        UserSelectionComponent,
-        StartComponent,
-        BalanceOverviewComponent,
-        TransactionHistoryComponent,
-        DashboardComponent,
-        LoginComponent,
-        UserSelectionComponent,
-        SpendingsComponent,
-        CommonModule,
-        LayoutComponent,
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    providers: [UserService],
+  imports: [CommonModule, SharedModule],
+  declarations: [
+    UserSelectionComponent,
+    StartComponent,
+    BalanceOverviewComponent,
+    TransactionHistoryComponent,
+    DashboardComponent,
+    LoginComponent,
+    UserSelectionComponent,
+    SpendingsComponent,
+  ],
+  exports: [
+    UserSelectionComponent,
+    StartComponent,
+    BalanceOverviewComponent,
+    TransactionHistoryComponent,
+    DashboardComponent,
+    LoginComponent,
+    UserSelectionComponent,
+    SpendingsComponent,
+    CommonModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [UserService],
 })
 export class PagesModule {}
