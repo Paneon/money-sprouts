51668bdf4bc62e4fd400937f8f117c0a4d5bedee
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2023-08-10 10:09:27 +0200

    #38 change logout path to actually trigger BE logout

--- apps/client/src/shared/page-header/page-header.component.ts
+++ apps/client/src/shared/page-header/page-header.component.ts
@@ -1,98 +1,103 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { UserService } from '../../app/services/user.service';
-import { Observable, Subject, debounceTime, distinctUntilChanged, filter, map, takeUntil } from 'rxjs';
+import {
+    Observable,
+    Subject,
+    debounceTime,
+    distinctUntilChanged,
+    filter,
+    map,
+    takeUntil,
+} from 'rxjs';
 import { User } from '@money-sprouts/shared/domain';
 
 @Component({
-  selector: 'money-sprouts-page-header',
-  templateUrl: './page-header.component.html',
-  styleUrls: ['./page-header.component.scss'],
+    selector: 'money-sprouts-page-header',
+    templateUrl: './page-header.component.html',
+    styleUrls: ['./page-header.component.scss'],
 })
 export class PageHeaderComponent implements OnInit, OnDestroy {
-  childClass: string;
-  username: string;
-  user$: Observable<User | null>
-  urlSegment: string;
-  logout = 'Logout';
-  avatar: string;
-  id: number;
-  isLoading = false;
+    childClass: string;
+    username: string;
+    user$: Observable<User | null>;
+    urlSegment: string;
+    logout = 'Logout';
+    avatar: string;
+    id: number;
+    isLoading = false;
 
-  private destroy$ = new Subject<void>();
+    private destroy$ = new Subject<void>();
 
-
-  constructor(
-    private router: Router,
-    public userService: UserService,
-    ) {
-      this.userService.loading.subscribe(loading => {
-        this.isLoading = loading;
-      });
+    constructor(private router: Router, public userService: UserService) {
+        this.userService.loading.subscribe((loading) => {
+            this.isLoading = loading;
+        });
     }
 
-  ngOnInit() {
-    const urlSegments = this.router.url.split('/');
-    this.urlSegment = urlSegments[urlSegments.length - 1];
-    this.username = urlSegments[2];
+    ngOnInit() {
+        const urlSegments = this.router.url.split('/');
+        this.urlSegment = urlSegments[urlSegments.length - 1];
+        this.username = urlSegments[2];
 
-    this.user$ = this.userService.currentUser$.pipe(
-      distinctUntilChanged()
-    );   
+        this.user$ = this.userService.currentUser$.pipe(distinctUntilChanged());
 
-    this.router.events
-      .pipe(
-        filter(event => event instanceof NavigationEnd),
-        map(() => this.router.url.split('/')[2]),
-        distinctUntilChanged(),
-        takeUntil(this.destroy$)
-      )
-       .subscribe(username => {
-        console.log(console.log('User in header: ', username))
-        this.username = username;
-        this.userService.getUserByUsername(username)
-      });
-    
-  }
-
-  goBack() {
-      if (this.urlSegment === 'dashboard') {
-          this.router.navigate(['userselection']);
-      } else if (this.urlSegment === 'history' || this.urlSegment === 'plan' || this.urlSegment === 'overview'){
-        this.router.navigate([`user/${this.username}/dashboard`]);
-      } else {
-        return;
-      }
-  }
-
-  goToUserselection() {
-    this.router.navigate(['userselection']);
-  }
-
-  get pageTitle(): string {
-    const pageName = this.router.url.split('/')[3];
-    switch (pageName) {
-      case 'dashboard':
-        return 'Dashboard';
-      case 'overview':
-        return 'Overview';
-      case 'history':
-        return 'History';
-      case 'plan':
-        return 'Plan';
-      default:
-        return '';
+        this.router.events
+            .pipe(
+                filter((event) => event instanceof NavigationEnd),
+                map(() => this.router.url.split('/')[2]),
+                distinctUntilChanged(),
+                takeUntil(this.destroy$)
+            )
+            .subscribe((username) => {
+                console.log(console.log('User in header: ', username));
+                this.username = username;
+                this.userService.getUserByUsername(username);
+            });
     }
-  }
 
-  onLogout(event: Event) {
-    event.preventDefault();
-    this.userService.logoutOrDeselectUser();
-    this.router.navigate(['/']);
-  }
+    goBack() {
+        if (this.urlSegment === 'dashboard') {
+            this.router.navigate(['userselection']);
+        } else if (
+            this.urlSegment === 'history' ||
+            this.urlSegment === 'plan' ||
+            this.urlSegment === 'overview'
+        ) {
+            this.router.navigate([`user/${this.username}/dashboard`]);
+        } else {
+            return;
+        }
+    }
 
-  ngOnDestroy(): void {
-      this.destroy$.next();
-      this.destroy$.complete();
-  }
+    goToUserselection() {
+        this.router.navigate(['userselection']);
+    }
+
+    get pageTitle(): string {
+        const pageName = this.router.url.split('/')[3];
+        switch (pageName) {
+            case 'dashboard':
+                return 'Dashboard';
+            case 'overview':
+                return 'Overview';
+            case 'history':
+                return 'History';
+            case 'plan':
+                return 'Plan';
+            default:
+                return '';
+        }
+    }
+
+    onLogout(event: Event) {
+        event.preventDefault();
+        this.userService.logoutOrDeselectUser();
+        this.router.navigate(['/logout']);
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
 }
