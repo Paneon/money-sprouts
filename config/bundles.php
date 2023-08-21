b83ce8f33cf1bdebe609495e880b665a2c93b1ec
Macavity <300609+Macavity@users.noreply.github.com>
2023-08-21 12:05:31 +0200

    Remove local packages

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
-//    Paneon\PhpToTypeScriptBundle\PhpToTypeScriptBundle::class => ['all' => true],
+    Paneon\PhpToTypeScriptBundle\PhpToTypeScriptBundle::class => ['all' => true],
 ];
