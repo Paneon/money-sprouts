d0de968c20fae95f5492a6ca9145d7545aef377b
Macavity <300609+Macavity@users.noreply.github.com>
2024-01-17 12:03:23 +0100

    Add AssetPathExtractor
    - AssetPathExtractor reads the generated public/build/index.html to enable the twig template to refer to the generated asset files
    - adjusted the production build for angular to use outputHashing and generate source maps

---
+++ src/Service/AssetPathExtractor.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+
+class AssetPathExtractor
+{
+    private array $javascriptFiles = [];
+    private array $cssFiles = [];
+
+    public function __construct(string $indexPath, string $buildDir, LoggerInterface $logger)
+    {
+        $content = file_get_contents($indexPath);
+        preg_match_all('/<script src="(.+?)" type="module"><\/script>/', $content, $matches);
+        foreach ($matches[1] as $path) {
+            $baseName = $this->extractBaseName($path, 'js');
+            $this->javascriptFiles[$baseName] = $buildDir . $path;
+        }
+
+        preg_match_all('/<link rel="stylesheet" href="(.+?)">/', $content, $matches);
+        foreach ($matches[1] as $path) {
+            $baseName = $this->extractBaseName($path, 'css');
+            $this->cssFiles[$baseName] = $buildDir . $path;
+        }
+    }
+
+    /**
+     * Returns the base name without the hash which is used for production builds
+     */
+    private function extractBaseName(string $path, string $suffix): string
+    {
+        // Assuming file names are in the format 'name.hash.js'
+        if (preg_match('/(?<basename>[^"]*?)(?<hash>\.[a-f0-9]*)?\.' . $suffix . '/', $path, $matches)) {
+            return $matches[1];
+        }
+
+        return basename($path, '.' . $suffix); // Fallback for non-hashed file names
+    }
+
+    public function getScriptPath(string $name): ?string
+    {
+        return $this->javascriptFiles[$name] ?? null;
+    }
+
+    public function getCssPath(string $name): ?string
+    {
+        return $this->cssFiles[$name] ?? null;
+    }
+}
