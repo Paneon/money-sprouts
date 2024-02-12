17755f1a19bba3ab9934467aadb6ea764f294eaa
Macavity <300609+Macavity@users.noreply.github.com>
2024-02-12 18:52:21 +0100

    delete account-selection, dashboard and login as they have been migrated

--- apps/client/src/app/pages/login/login.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'money-sprouts-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-})
-export class LoginComponent {}
