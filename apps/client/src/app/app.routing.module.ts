88988befb0b5b823e748eb56cc6cf31860d6ee4c
Macavity <300609+Macavity@users.noreply.github.com>
2024-02-12 18:56:44 +0100

    fix: angular build

--- apps/client/src/app/app.routing.module.ts
+++ apps/client/src/app/app.routing.module.ts
@@ -4,31 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BalanceOverviewComponent } from './pages/balance-overview/balance-overview.component';
 import { TransactionHistoryComponent } from './pages/transaction-history/transaction-history.component';
-import { LoginComponent } from './pages/login/login.component';
 import { PlanComponent } from './pages/plan/plan.component';
-import { AccountSelectionComponent } from './pages/account-selection/account-selection.component';
-import { AccountsResolver } from './services/accounts-resolver.service';
 import { RoutePath } from './enum/routepath';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
     {
         path: '',
         redirectTo: RoutePath.Home,
         pathMatch: 'full',
-    },
-    {
-        path: RoutePath.Login,
-        component: LoginComponent,
-    },
-    {
-        path: RoutePath.AccountSelection,
-        component: AccountSelectionComponent,
-        resolve: { accounts: AccountsResolver },
-    },
-    {
-        path: RoutePath.Dashboard,
-        component: DashboardComponent,
     },
     {
         path: RoutePath.Overview,
--- apps/client/src/app/pages/pages.module.ts
+++ apps/client/src/app/pages/pages.module.ts
@@ -1,11 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AccountSelectionComponent } from './account-selection/account-selection.component';
 import { SharedModule } from '../../shared/shared.module';
 import { BalanceOverviewComponent } from './balance-overview/balance-overview.component';
 import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
 import { PlanComponent } from './plan/plan.component';
 import { AccountService } from '../services/account.service';
 import { PlanExpensesComponent } from './plan/plan-expenses/plan-expenses.component';
@@ -16,21 +13,15 @@ import { TranslateModule } from '@ngx-translate/core';
 @NgModule({
     imports: [CommonModule, SharedModule, FormsModule, TranslateModule],
     declarations: [
-        AccountSelectionComponent,
         BalanceOverviewComponent,
         TransactionHistoryComponent,
-        DashboardComponent,
-        LoginComponent,
         PlanComponent,
         PlanExpensesComponent,
         PlanEarningsComponent,
     ],
     exports: [
-        AccountSelectionComponent,
         BalanceOverviewComponent,
         TransactionHistoryComponent,
-        DashboardComponent,
-        LoginComponent,
         PlanComponent,
         PlanExpensesComponent,
         PlanEarningsComponent,
